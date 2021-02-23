febf4ddae7c40f1d86e80ff2da76485f661a4935
EvanRPavone <evanrpavone@gmail.com>
2021-02-22 19:10:26 -0500

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects, only: [:create, :index, :show] do
+    resources :comments, only: [:create, :index]
+  end
+  resources :users, only: [:create]
+  post '/login', to: 'sessions#create'
+  get '/authorize', to: 'sessions#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
