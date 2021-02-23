25db2aba05be6b143c9e1e1d42fca11bfc049c45
EvanRPavone <evanrpavone@gmail.com>
2021-02-22 19:09:43 -0500

    controllers setup

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,20 @@
+class ProjectsController < ApplicationController
+    def create 
+        Project.create(project_params)
+        render json: { message: 'project saved'}
+    end
+    
+    def index
+        projects = Project.all 
+        render json: projects
+    end
+    
+private 
+    def project_params
+        params.require(:project).permit(:title, 
+        :description, 
+        :language, 
+        :github_url, 
+        :demo_url)
+    end
+end
